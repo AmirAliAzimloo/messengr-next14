f6d045a38e2d983d6d4e41c51ba2efd92c8a88bf
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-17 00:49:39 +0330

    credential login completed

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios"
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react"
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -39,10 +41,25 @@ const AuthForm = () => {
 
     if (variant === "REGISTER") {
       axios.post("/api/register",data)
+        .catch(()=>toast.error("Something went wrong!"))
+        .finally(()=>setIsLoading(false))
     }
 
     if (variant === "LOGIN") {
-      // NextAuth Signin
+      signIn("credentials",{
+        ...data,
+        redirect:false
+      })
+      .then((callback)=>{
+        if(callback?.error){
+          toast.error("Invalid credentials")
+        }
+
+        if(callback?.ok && !callback?.error){
+          toast.success("Logged in!")
+        }
+      })
+      .finally(()=>setIsLoading(false))
     }
 
     const socialAction = (action: string) => {
