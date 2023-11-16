7b4c4df04b82ac512e1a3a637f209b806f13e7cd
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-16 21:49:56 +0330

    popular config and base ui/ux completed

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,6 +4,8 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from "react-icons/bs"
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -70,43 +72,111 @@ const AuthForm = () => {
       >
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
           {variant === "REGISTER" && (
-            <Input
-              id="name"
-              label="Name"
-              register={register}
-              errors={errors}
-            />
+            <Input id="name" label="Name" register={register} errors={errors} />
           )}
-           <Input
-              id="email"
-              label="Email address"
-              type="email"
-              register={register}
-              errors={errors}
-            />
-           <Input
-              id="password"
-              label="password"
-              type="password"
-              register={register}
-              errors={errors}
-            />
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
 
-            <div>
-                <Button
-                disabled={isLoading}
-                fullWidth
-                type="submit"
-
-                >
-                    {variant === "LOGIN" ? "Sign In" : 'Register'}
-                </Button>
-            </div>
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
         </form>
 
-         {/* sign in with social */}
+        {/* sign in with social */}
 
+        <div className="mt-6">
 
+            {/* divider */}
+          <div className="relative">
+            <div
+              className="
+            absolute
+            inset-0
+            flex
+            items-center
+            "
+            >
+              <div
+                className="
+              w-full 
+              border-t 
+              border-gray-300
+               "
+              />
+            </div>
+
+            <div
+              className=" 
+            relative 
+            flex 
+            justify-center 
+            text-sm "
+            >
+              <span
+                className="
+              bg-white 
+              px-2 
+              text-gray-500"
+              >
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+            {/* social */}
+          <div className="mt-6 flex gap-2" >
+            <AuthSocialButton 
+            icon={BsGithub}
+            onClick={()=>socialAction("github")}
+            />
+
+            <AuthSocialButton 
+            icon={BsGoogle}
+            onClick={()=>socialAction("google")}
+            />
+          </div>
+
+          {/* toggle auth */}
+          <div className="
+          flex
+          gap-2
+          justify-center
+          text-sm
+          mt-6 
+          px-2
+          text-gray-500
+          " >
+
+            <div>
+            { variant === "LOGIN" ? "New to Messanger?" : "Already have an account?" }
+            </div>
+            <div
+            onClick={toggleVariant}
+            className="
+            underline
+            cursor-pointer
+            "
+            >
+            { variant === "LOGIN" ? "Create an account" : "Login" }
+            </div>
+
+          </div>
+
+        </div>
       </div>
     </div>
   );
