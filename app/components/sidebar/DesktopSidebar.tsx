c24ae7b263ecd7f89242750a262bffd88823cfe9
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-17 22:30:04 +0330

    self user chat completed & chat list started

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
 import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
-interface DesktopSidebarProps{
-    currentUser:User
+interface DesktopSidebarProps {
+  currentUser: User;
 }
 
-const DesktopSidebar: React.FC<DesktopSidebarProps> = ({currentUser}) => {
-    const routes = useRoutes();
-    const [isOpen,setIsOpen] = useState<boolean>(false)
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-
-    return ( 
-        <div
-        className="
+  return (
+    <div
+      className="
         hidden
         lg:fixed
         lg:inset-y-0
@@ -32,32 +32,52 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({currentUser}) => {
         lg:flex-col
         justify-between
         "
-        >
-            <nav className="mt-4 flex flex-col justify-between">
-            <ul
-            role="list"
-            className="
+    >
+      <nav className="mt-4 flex flex-col justify-between">
+        <ul
+          role="list"
+          className="
             flex
             flex-col
             items-center
             space-y-1
             "
-            >
-            {routes.map((item)=>(
-                <DesktopItem
-                key={item.label}
-                href={item.href}
-                label={item.label}
-                icon={item.icon}
-                active={item.active}
-                onClick={item.onClick}
-              />
-            ))}
-            </ul>
-            </nav>
-           
+        >
+          {routes.map((item) => (
+            <DesktopItem
+              key={item.label}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              active={item.active}
+              onClick={item.onClick}
+            />
+          ))}
+        </ul>
+      </nav>
+
+      <nav
+        className="
+         mt-4 
+         flex 
+         flex-col 
+         justify-between
+         items-center
+         "
+      >
+        <div
+          onClick={() => setIsOpen(true)}
+          className="
+           cursor-pointer
+           hover:opacity-75
+           transition
+           "
+        >
+          <Avatar user={currentUser} />
         </div>
-     );
-}
- 
+      </nav>
+    </div>
+  );
+};
+
 export default DesktopSidebar;
--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
         swcPlugins:[
             ["next-superjson-plugin",{}]
         ]
-    }
+    },
+    images: {
+        domains: [
+          'res.cloudinary.com', 
+          'avatars.githubusercontent.com',
+          'lh3.googleusercontent.com'
+        ]
+      }
 }
 
 module.exports = nextConfig
