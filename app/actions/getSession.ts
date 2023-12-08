4ff133f5a26b96e8654edaac9c0934428d7dfe7a
Amir Ali <amiraliazimloo123@gmail.com>
2023-12-08 16:18:00 +0330

    fix build time bugs

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/libs/nextAuth";
 
 export default async function getSession() {
     return await getServerSession(authOptions)
--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { getServerSession } from "next-auth";
 
 import { pusherServer } from "@/app/libs/pusher";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/libs/nextAuth";
 
 export default async function handler(
   request: NextApiRequest, 
