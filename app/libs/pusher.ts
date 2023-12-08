6b3425351786b2f7c4eee0b49b629342b05443b7
Amir Ali <amiraliazimloo123@gmail.com>
2023-12-08 12:58:10 +0330

    fix some bugs

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -1,6 +1,7 @@
 import PusherServer from 'pusher'
 import PusherClient from 'pusher-js'
 
+
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
